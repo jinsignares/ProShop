5fc6be345c14c5c31ead54b600ba4b8f71366d21
foreverajohn <58446479+foreverajohn@users.noreply.github.com>
2021-09-01 12:10:37 -0500

    34. Cart Screen

--- frontend/src/redux/actions/cartActions.js
+++ frontend/src/redux/actions/cartActions.js
@@ -2,16 +2,16 @@ import axios from 'axios'
 import { CART_ADD_ITEM, CART_REMOVE_ITEM } from "../actions/types"
 
 export const addToCart = (id, qty) => async (dispatch, getState) => {
-    const { data } = await axios.get(`/api/products/${id}`)
+    const { data: { _id, name, image, price, countInStock } } = await axios.get(`/api/products/${id}`)
 
     dispatch({
         type: CART_ADD_ITEM,
         payload: {
-            product: data._id,
-            name: data.name,
-            image: data.image,
-            price: data.price,
-            countInStock: data.countInStock,
+            product: _id,
+            name,
+            image,
+            price,
+            countInStock,
             qty
         }
     })
