6f60d4d66876bb1497acffd3e1c5a1478cf80024
foreverajohn <58446479+foreverajohn@users.noreply.github.com>
2021-09-01 15:46:50 -0500

    48. Profile Screen & Get User Details

--- frontend/src/redux/reducers/userReducers.js
+++ frontend/src/redux/reducers/userReducers.js
@@ -1,4 +1,4 @@
-import { USER_LOGIN_FAIL, USER_LOGIN_REQUEST, USER_LOGIN_SUCCESS, USER_LOGOUT, USER_REGISTER_FAIL, USER_REGISTER_REQUEST, USER_REGISTER_SUCCESS } from "../actions/types"
+import { USER_DETAILS_FAIL, USER_DETAILS_REQUEST, USER_DETAILS_SUCCESS, USER_LOGIN_FAIL, USER_LOGIN_REQUEST, USER_LOGIN_SUCCESS, USER_LOGOUT, USER_REGISTER_FAIL, USER_REGISTER_REQUEST, USER_REGISTER_SUCCESS } from "../actions/types"
 
 
 export const userLoginReducer = (state = {}, action) => {
@@ -27,6 +27,21 @@ export const userRegisterReducer = (state = {}, action) => {
         case USER_REGISTER_SUCCESS:
             return { loading: false, userInfo: payload }
         case USER_REGISTER_FAIL:
+            return { loading: false, error: payload }
+        default:
+            return state
+    }
+}
+
+export const userDetailsReducer = (state = { user: {} }, action) => {
+    const { type, payload } = action
+
+    switch (type) {
+        case USER_DETAILS_REQUEST:
+            return { ...state, loading: true }
+        case USER_DETAILS_SUCCESS:
+            return { loading: false, user: payload }
+        case USER_DETAILS_FAIL:
             return { loading: false, error: payload }
         default:
             return state
