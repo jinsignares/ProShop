28d3d2ce5758d5050350755d67edaca0d0b4ecde
foreverajohn <58446479+foreverajohn@users.noreply.github.com>
2021-09-01 17:12:40 -0500

    54. Place Order Screen

--- frontend/src/screens/ShippingScreen.js
+++ frontend/src/screens/ShippingScreen.js
@@ -23,7 +23,7 @@ const ShippingScreen = ({ history }) => {
 
     return (
         <FormContainer>
-            <CheckOutSteps step1 step2 />
+            <CheckOutSteps step1 step2 step3 />
             <h1>Shipping</h1>
             <Form onSubmit={submitHandler}>
                 <Form.Group controlId='address'>
