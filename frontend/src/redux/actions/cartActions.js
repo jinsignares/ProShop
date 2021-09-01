588cb7fdd4bdfe4dee2b086203734f0d3f735433
foreverajohn <58446479+foreverajohn@users.noreply.github.com>
2021-09-01 11:02:30 -0500

    32. Cart Reducer & Add To Cart Action

---
+++ frontend/src/redux/actions/cartActions.js
@@ -0,0 +1,20 @@
+import axios from 'axios'
+import { CART_ADD_ITEM, CART_REMOVE_ITEM } from "../actions/types"
+
+export const addToCart = (id, qty) => async (dispatch, getState) => {
+    const { data: { _id, name, image, price, countInStock } } = await axios.get(`/api/producst/${id}`)
+
+    dispatch({
+        type: CART_ADD_ITEM,
+        payload: {
+            product: _id,
+            name,
+            image,
+            price,
+            countInStock,
+            qty
+        }
+    })
+
+    localStorage.setItem('cartItems', JSON.stringify(getState().cart.cartItems))
+}
