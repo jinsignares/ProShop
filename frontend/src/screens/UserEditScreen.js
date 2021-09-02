61b0704347601ece0943889b317d27cc1e9c1a84
foreverajohn <58446479+foreverajohn@users.noreply.github.com>
2021-09-02 17:58:50 -0500

    72. Update User Functionality

--- frontend/src/screens/UserEditScreen.js
+++ frontend/src/screens/UserEditScreen.js
@@ -5,9 +5,10 @@ import { useDispatch, useSelector } from "react-redux"
 import Message from "../components/Message"
 import Loader from '../components/Loader'
 import FormContainer from '../components/FormContainer'
-import { getUserDetails } from '../redux/actions/userActions'
+import { getUserDetails, updateUser } from '../redux/actions/userActions'
+import { USER_UPDATE_RESET } from "../redux/actions/types"
 
-const UserEditScreen = ({ match }) => {
+const UserEditScreen = ({ match, history }) => {
     const userId = match.params.id
 
     const [name, setName] = useState('')
@@ -19,19 +20,32 @@ const UserEditScreen = ({ match }) => {
     const userDetails = useSelector(state => state.userDetails)
     const { loading, error, user } = userDetails
 
+    const userUpdate = useSelector(state => state.userUpdate)
+    const { loading: loadingUpdate, error: errorUpdate, success: successUpdate } = userUpdate
+
     useEffect(() => {
-        if (!user.name || user._id !== userId) {
-            dispatch(getUserDetails(userId))
+        if (successUpdate) {
+            dispatch({ type: USER_UPDATE_RESET })
+            history.push('/admin/userlist')
         } else {
-            setName(user.name)
-            setEmail(user.email)
-            setIsAdmin(user.isAdmin)
+            if (!user.name || user._id !== userId) {
+                dispatch(getUserDetails(userId))
+            } else {
+                setName(user.name)
+                setEmail(user.email)
+                setIsAdmin(user.isAdmin)
+            }
         }
-    }, [user, dispatch, userId])
+    }, [user, dispatch, userId, successUpdate, history])
 
     const submitHandler = (e) => {
         e.preventDefault()
-
+        dispatch(updateUser({
+            _id: userId,
+            name,
+            email,
+            isAdmin
+        }))
     }
 
     return (
@@ -39,6 +53,8 @@ const UserEditScreen = ({ match }) => {
             <Link to='/admin/userlist' className='btn btn-light my-3'>Go Back</Link>
             <FormContainer>
                 <h1>Edit User</h1>
+                {loadingUpdate && <Loader />}
+                {errorUpdate && <Message variant='danger'>{errorUpdate}</Message>}
                 {loading ? <Loader /> : error ? <Message variant='danger'>{error}</Message> : (
                     <Form onSubmit={submitHandler}>
                         <Form.Group controlId='name'>
