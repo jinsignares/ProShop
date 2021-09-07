e15833cd2962fae0a3a98ee6689cfaef5140eb30
foreverajohn <58446479+foreverajohn@users.noreply.github.com>
2021-09-07 11:24:54 -0500

    78. Admin Update Product

--- frontend/src/redux/reducers/productReducers.js
+++ frontend/src/redux/reducers/productReducers.js
@@ -1,4 +1,4 @@
-import { PRODUCT_CREATE_FAIL, PRODUCT_CREATE_REQUEST, PRODUCT_CREATE_RESET, PRODUCT_CREATE_SUCCESS, PRODUCT_DELETE_FAIL, PRODUCT_DELETE_REQUEST, PRODUCT_DELETE_SUCCESS, PRODUCT_DETAILS_FAIL, PRODUCT_DETAILS_REQUEST, PRODUCT_DETAILS_SUCCESS, PRODUCT_LIST_FAIL, PRODUCT_LIST_REQUEST, PRODUCT_LIST_SUCCESS } from "../actions/types"
+import { PRODUCT_CREATE_FAIL, PRODUCT_CREATE_REQUEST, PRODUCT_CREATE_RESET, PRODUCT_CREATE_SUCCESS, PRODUCT_DELETE_FAIL, PRODUCT_DELETE_REQUEST, PRODUCT_DELETE_SUCCESS, PRODUCT_DETAILS_FAIL, PRODUCT_DETAILS_REQUEST, PRODUCT_DETAILS_SUCCESS, PRODUCT_LIST_FAIL, PRODUCT_LIST_REQUEST, PRODUCT_LIST_SUCCESS, PRODUCT_UPDATE_FAIL, PRODUCT_UPDATE_REQUEST, PRODUCT_UPDATE_RESET, PRODUCT_UPDATE_SUCCESS } from "../actions/types"
 
 export const productListReducer = (state = { products: [] }, action) => {
     const { type, payload } = action
@@ -57,6 +57,23 @@ export const productCreateReducer = (state = {}, action) => {
             return { loading: false, error: payload }
         case PRODUCT_CREATE_RESET:
             return {}
+        default:
+            return state
+    }
+}
+
+export const productUpdateReducer = (state = { product: {} }, action) => {
+    const { type, payload } = action
+
+    switch (type) {
+        case PRODUCT_UPDATE_REQUEST:
+            return { loading: true }
+        case PRODUCT_UPDATE_SUCCESS:
+            return { loading: false, success: true, product: payload }
+        case PRODUCT_UPDATE_FAIL:
+            return { loading: false, error: payload }
+        case PRODUCT_UPDATE_RESET:
+            return { product: {} }
         default:
             return state
     }
